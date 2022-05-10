7cf00674f5df6f11abdab88650ca4448b3597829
HengKanitha <kanitha.heng@ecoinsoft.com>
2022-05-10 14:22:01 +0700

    add Product to extends and add method customer and product

--- Stored/Customer.java
+++ Stored/Customer.java
@@ -1,4 +1,16 @@
 package Stored;
 
 public class Customer {
+int cus_id;
+String cus_name;
+String cus_gender;
+String address;
+String phone;
+public void customers(int cus_id,String cus_name,String cus_gender,String address,String phone){
+    this.cus_id=cus_id;
+    this.cus_name=cus_name;
+    this.cus_gender=cus_gender;
+    this.address=address;
+    this.phone=phone;
+}
 }
--- Stored/Product.java
+++ Stored/Product.java
@@ -1,4 +1,14 @@
 package Stored;
 
-public class Product {
+public class Product extends Customer{
+    int pro_id;
+    String pro_name;
+    int qty;
+    float price;
+     public void product(int pro_id, String pro_name, int qty, float price){
+         this.pro_id=pro_id;
+         this.pro_name=pro_name;
+         this.qty=qty;
+         this.price=price;
+     }
 }
