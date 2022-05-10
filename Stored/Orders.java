5d0df39f09682dd145b12b739e5db5d3e9d4050f
HengKanitha <kanitha.heng@ecoinsoft.com>
2022-05-10 14:34:45 +0700

    add class Orders

--- Stored/Orders.java
+++ Stored/Orders.java
@@ -5,8 +5,7 @@ public class Orders extends Product{
     int cus_id;
     int or_qty;
     float or_price;
-    public void order(int or_id, int cus_id, int or_qty,
-            float or_price){
+    public void order(int or_id, int cus_id, int or_qty,float or_price){
 
     }
 }
